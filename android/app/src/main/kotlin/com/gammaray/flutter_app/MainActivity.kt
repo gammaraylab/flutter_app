ec5faf72f07e9c86eb080af351d8d7e608f3a6cf
anadi mitra <gammarayelectron@gmail.com>
2020-10-29 16:48:43 +0530

    starting the first flutter app

---
+++ android/app/src/main/kotlin/com/gammaray/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gammaray.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("First app"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("hello world xd"),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+      },
+        child: Text("click"),
+      ),
+    ),
+  ));
+}
